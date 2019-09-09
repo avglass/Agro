750b145225a76bfdcfdb530fda6287b76a8b4079
Angela <angela.glass.avg@gmail.com>
2019-09-08 19:08:18 -0500

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+        <div>
+           <p>plz work</p>
+        </div>
+        );
+    }
+}
+export default Home;
